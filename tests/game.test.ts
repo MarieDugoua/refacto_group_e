b26fef7bcc9bacf988e47820e230e82898259d94
yohannmarchand <marchandyohannnnn@gmail.com>
2023-03-03 16:03:37 +0100

    test: 💍 should not have less then 2 players to play a game

--- tests/game.test.ts
+++ tests/game.test.ts
@@ -1,6 +1,8 @@
 import {expect} from 'chai';
 import {describe, it} from 'mocha';
 import {GameRunner} from '../src/game-runner';
+import {Game} from "../src/game";
+import NotEnoughtPlayerError from "../src/NotEnoughtPlayerError";
 
 describe('The test environment', () => {
     it('should pass', () => {
@@ -11,4 +13,17 @@ describe('The test environment', () => {
         expect(GameRunner).to.not.be.undefined;
     });
 
+    it("should not have less then 2 players to play the game", () => {
+        const game = new Game()
+
+        expect(() => game.roll(5)).to.throw(NotEnoughtPlayerError)
+
+        game.add('Pet')
+
+        expect(() => game.roll(5)).to.throw(NotEnoughtPlayerError)
+
+        game.add('Ed')
+
+        expect(() => game.roll(5)).not.to.throw(NotEnoughtPlayerError)
+    })
 });
